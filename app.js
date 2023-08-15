defa3920dc6ec06bd09ab46c0f4617c8b6476a05
Doeon Kim <keyfrog@outlook.kr>
2023-08-15 19:30:35 +0900

    감정과 동물 수정

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var emotion = ["기쁜", "슬픈", "행복한", "심심한", "졸린"]
+var emotion = ["기쁜", "슬픈", "행복한", "심심한", "졸린", "짜증난", "화난"]
 var color = ["빨간색", "주황색", "노란색", "초록색", "파란색", "남색", "보라색"]
-var animal = ["카피바라", "기린", "토끼", "호랑이", "사자", "말", "재규어", "고양이"]
+var animal = ["카피바라", "기린", "토끼", "호랑이", "사자", "말", "재규어", "고양이", "소", "돼지"]
 
 function getRandomElement(arr) {
     var randomIndex = Math.floor(Math.random() * arr.length)
